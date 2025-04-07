0fada9d78d2f77f293d48c991731d8671c547814
Arthur Santos <a4hur78@gmail.com>
2025-04-07 17:02:23 -0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,8 @@ import {
 import './index.css'
 import App from './App.jsx'
 import Contact from './Contact.jsx'
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const router = createBrowserRouter([
   {
@@ -21,6 +23,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <SpeedInsights/>
+    <Analytics/>
     <RouterProvider router={router} />
     
   </StrictMode>,
